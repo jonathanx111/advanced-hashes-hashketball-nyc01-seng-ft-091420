cf30f95acef99a76faf5fe9a474acfe81dc02db2
Jonathan Xiao <jonathanx111@gmail.com>
2020-09-13 02:22:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -141,5 +141,13 @@ def num_points_scored(player_name)
 end
 
 def shoe_size(player_name)
-  
+   game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      team_data[:players].each do |ele|
+        if ele[:player_name] == player_name
+          return ele[:shoe]
+        end
+      end
+    end
+  end
 end
