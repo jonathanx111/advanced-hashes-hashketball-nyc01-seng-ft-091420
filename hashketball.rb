41a4b787563505aee41cc0048d36eefb8a72eafc
Jonathan Xiao <jonathanx111@gmail.com>
2020-09-13 03:38:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,8 +187,8 @@ def player_stats(name)
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
       team_data[:players].each do |ele|
-        if ele[:player_name] == player_name
-          return ele[:points]
+        if ele[:player_name] == name
+          
         end
       end
     end
